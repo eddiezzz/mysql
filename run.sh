a46121916b2765d794c98c91649f0dc2309604b6
eddie <zcs5612@163.com>
2018-11-14 16:50:22 +0800

    update:

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 cur=$(pwd)
-docker stop mysql
-docker rm mysql
+
+sh -x stop.sh
+sh -x clear.sh
 
 docker run --name mysql -d -p 3306:3306 -v $cur/volume/etc/mysql:/etc/mysql -v $cur/volume/data:/var/lib/mysql -v /etc/localtime:/etc/localtime:ro -v init:/init -e MYSQL_ALLOW_EMPTY_PASSWORD=yes mysql:5.7.18
 if [ $? -ne 0 ]; then
--- stop.sh
+++ stop.sh
@@ -1,5 +1,3 @@
 cur=$(pwd)
 docker stop mysql
 docker rm mysql
-
-docker run --name mysql -d -p 3306:3306 -v $cur/volume/etc/mysql:/etc/mysql -v $cur/volume/data:/var/lib/mysql -v /etc/localtime:/etc/localtime:ro -v init:/init -e MYSQL_ALLOW_EMPTY_PASSWORD=yes mysql:5.7.18
