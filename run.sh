a93f6b4dcc76a4af1e5ccd804707f9f4350910b3
eddie <zhengchangshuai@mobike.com>
2018-11-17 10:19:04 +0800

    update

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@ cur=$(pwd)
 sh -x stop.sh
 sh -x clear.sh
 
+with_time="-v /etc/localtime:/etc/localtime:ro"
 os=$(uname)
-withTime=""
 if [ "-$os" = "-Darwin" ]; then
-    withTime="-v /etc/localtime:/etc/localtime:ro"
+    with_time=""
 fi
 
-docker run --name mysql -d -p 3306:3306 -v $cur/volume/etc/mysql:/etc/mysql -v $cur/volume/data:/var/lib/mysql -v init:/init $withTime -e MYSQL_ALLOW_EMPTY_PASSWORD=yes mysql:5.7.18
+docker run --name mysql -d -p 3306:3306 -v $cur/volume/etc/mysql:/etc/mysql -v $cur/volume/data:/var/lib/mysql -v init:/init $with_time -e MYSQL_ALLOW_EMPTY_PASSWORD=yes mysql:5.7.18
 if [ $? -ne 0 ]; then
 	echo "docker run error, check it"
 	exit 1
